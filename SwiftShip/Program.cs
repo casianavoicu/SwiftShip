cdaf883d667c8310ff7e8c23ac6ebc63825b2303
Casiana Voicu <casiana.voicu@bringingweb.com>
2023-10-29 23:22:02 +0200

    fixed navbar

--- SwiftShip/Program.cs
+++ SwiftShip/Program.cs
@@ -1,23 +1,22 @@
 using Microsoft.EntityFrameworkCore;
-using SwiftShip.Data;
-using SwiftShip.Database;
-using SwiftShip.Service;
 using SwiftShip.BusinessLogic;
 using SwiftShip.BusinessLogic.Mapper;
-using DevExpress.XtraCharts;
+using SwiftShip.Database;
+using SwiftShip.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<IParcelBusinessLogic, ParcelBusinessLogic>();
 builder.Services.AddScoped<IParcelStageHistoryBusinessLogic, ParcelStageHistoryBusinessLogic>();
 builder.Services.AddSingleton<IStageBusinessLogic, StageBusinessLogic>();
 builder.Services.AddAutoMapper(typeof(ParcelMapperProfile));
 builder.Services.AddDbServices();
 builder.Services.AddDevExpressBlazor();
+//builder.WebHost.UseWebRoot("wwwroot");
+//builder.WebHost.UseStaticWebAssets();
 builder.Services.AddDbContext<SwiftShipDbContext>
            (options => options.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionString")));
 
